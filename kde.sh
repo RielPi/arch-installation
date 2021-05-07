a093d97e437a81e3657834cf5ab877f27160a17b
RielPi <83147796+RielPi@users.noreply.github.com>
2021-05-07 17:41:25 +0000

    Update kde.sh
    package added: powerdevil
    package removed: user-manager

--- kde.sh
+++ kde.sh
@@ -3,18 +3,8 @@
 # kde
 echo "exec startkde" > /home/$USER/.xinitrc
 sudo pacman -S plasma-{browser-integration,desktop,disks,firewall,systemmonitor,workspace{,-wallpapers},pa,nm} breeze-gtk kde{-gtk-config,plasma-addons} k{screen,win,writed} sddm sddm-kcm
-sudo pacman -S kdialog khelpcenter khotkeys user-manager xdg-desktop-portal-kde kdegraphics-thumbnailers 
+sudo pacman -S kdialog khelpcenter khotkeys xdg-desktop-portal-kde kdegraphics-thumbnailers powerdevil
 # plasma5-applets-redshift-control
-
-echo "[General]
-Numlock=on
-
-[Theme]
-# Current theme name
-Current=breeze
-
-# Cursor theme used in the greeter
-CursorTheme=breeze_cursors" >> /etc/sddm.conf
 
 sudo systemctl enable sddm
 
@@ -24,5 +14,16 @@ SingleClick=false" >> ~/.config/kdeglobals
 # applications
 sudo pacman -S ark dolphin dolphin-plugins filelight gwenview kwrite k{calc,deconnect,floppy,runner,onsole} partitionmanager python-nautilus okular spectacle transmission-qt
 
+echo " Add the following config in /etc/sddm.conf
+[General]
+Numlock=on
+
+[Theme]
+# Current theme name
+Current=breeze
+
+# Cursor theme used in the greeter
+CursorTheme=breeze_cursors"
+echo "------------------------"
 echo "Kde plasma is installed!"
 echo "Reboot your system"
