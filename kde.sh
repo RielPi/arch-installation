f55730c82938cab94815da220c2f294feec3ae78
RielPi <83147796+RielPi@users.noreply.github.com>
2021-05-07 16:45:01 +0000

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -3,7 +3,7 @@
 # kde
 echo "exec startkde" > /home/$USER/.xinitrc
 sudo pacman -S plasma-{browser-integration,desktop,disks,firewall,systemmonitor,workspace{,-wallpapers},pa,nm} breeze-gtk kde{-gtk-config,plasma-addons} k{screen,win,writed} sddm sddm-kcm
-sudo pacman -S kdialog khelpcenter khotkeys user-manager xdg-desktop-portal-kde kdegraphics-thumbnailers
+sudo pacman -S kdialog khelpcenter khotkeys user-manager xdg-desktop-portal-kde kdegraphics-thumbnailers 
 # plasma5-applets-redshift-control
 
 echo "[General]
@@ -22,4 +22,7 @@ echo "[KDE]
 SingleClick=false" >> ~/.config/kdeglobals
 
 # applications
-sudo pacman -S ark dolphin dolphin-plugins filelight gwenview kwrite k{calc,deconnect,floppy,runner,onsole} partitionmanager python-nautilus okular spectacle
+sudo pacman -S ark dolphin dolphin-plugins filelight gwenview kwrite k{calc,deconnect,floppy,runner,onsole} partitionmanager python-nautilus okular spectacle transmission-qt
+
+echo "Kde plasma is installed!"
+echo "Reboot your system"
