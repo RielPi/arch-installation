26e2f96c4e6a707bb16f9d55c476a6baec66b232
RielPi <83147796+RielPi@users.noreply.github.com>
2021-05-07 17:28:11 +0000

    Update packages.sh
    typo errors

--- packages.sh
+++ packages.sh
@@ -2,7 +2,11 @@
 sudo pacman -Syu
 
 # "essenstials"
-sudo pacman -S curl top htop hunspell{-es_any,-en_US} gnupg screenfetch ufw wget zsh zsh-completions
+sudo pacman -S curl htop hunspell{-es_any,-en_US} gnupg screenfetch ufw wget zsh zsh-completions
+# fonts
+sudo pacman -S ttf-{hack,anonymous-pro,dejavu,freefont,liberation,roboto,bitstream-vera,croscore,droid,ubuntu-font-family} lib32-fontconfig wqy-zenhei
+#yay -S ttf-{ms-fonts,monaco,emojione-color,twemoji-color} noto-fonts-emoji
+
 # ufw
 sudo enable ufw
 sudo systemctl enable ufw
@@ -15,13 +19,9 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-# fonts
-sudo pacman -S ttf-{hack,anonymous-pro,dejavu,freefont,liberation,roboto, bitstream-vera,croscore,droid,ubuntu-font-family} lib32-fontconfig wqy-zenhei
-#yay -S ttf-{ms-fonts,monaco,emojione-color,twemoji-color} noto-fonts-emoji
-
 # packages
-sudo pacman -S firefox
-paru -S cryptomator discord spotify obsidian-appimage
+sudo pacman -S firefox discord
+paru -S cryptomator spotify obsidian-appimage
 
 # gaming
 sudo pacman -S wine-staging wine_gecko wine-mono winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
