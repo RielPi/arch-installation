0339d0d0b5b6e4eeca2bd0bb5552d95e54a6fa95
Riel <83147796+RielPi@users.noreply.github.com>
2021-05-08 14:18:18 -0500

    Create packages-aur.sh

---
+++ packages-aur.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install paru
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+# paru config
+echo "uncomment #BottomUp and add SkipReview bellow it"
+read -n 1 -s -r -p "...Press any key to continue... "
+sudo nano /etc/paru.conf
+
+# key required for spotify (official site), it won't install without it
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
+paru -S bitwarden fluent-reader spotify obsidian-appimage
+
+# required by cryptomator
+#paru -S jdk jdk-adoptopenjdk davfs2 kio-fuse sshfs
+#paru -S cryptomator
