76b1829d60c415bd653d0c10d0f64d54c612df51
Riel <83147796+RielPi@users.noreply.github.com>
2021-05-08 22:07:52 -0500

    Update dwm.sh
    rofi add, xinitrc missing fixed

--- DE/dwm.sh
+++ DE/dwm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo pacman -S nitrogen redshift rofi xorg-xinit libx11 libxinerama libxft webkit2gtk
+
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo "Remove the last lines"
 read -n 1 -s -r -p "...Press any key to continue... "
@@ -13,14 +15,10 @@ echo "xrandr --output HDMI-0 --mode 1600x900 &" >> ~/.xinitrc
 echo "redshift -l 71:-10 -t 3600:3600 &" >> ~/.xinitrc
 echo "exec dwm" >> ~/.xinitrc
 
-sudo pacman -S nitrogen redshift xorg-xinit libx11 libxinerama libxft webkit2gtk
-
 mkdir -p ~/.config/dwm
 
 git clone git://git.suckless.org/st ~/.config/dwm/st
 git clone git://git.suckless.org/dwm ~/.config/dwm/dwm
-git clone git://git.suckless.org/dmenu ~/.config/dwm/dmenu
-
 
 cd ~/.config/dwm/st
 make clean
@@ -28,11 +26,6 @@ make
 sudo make install
 
 cd ~/.config/dwm/dwm
-make clean
-make
-sudo make install
-
-cd ~/.config/dwm/dmenu
 make clean
 make
 sudo make install
