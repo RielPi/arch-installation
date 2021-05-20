fcad7d1203bd03222a3e2098fbe3ed524391e95e
Riel <83147796+RielPi@users.noreply.github.com>
2021-05-19 19:20:56 -0500

    Update packages-aur.sh

--- packages-aur.sh
+++ packages-aur.sh
@@ -13,7 +13,7 @@ sudo nano /etc/paru.conf
 # key required for spotify (official site), it won't install without it
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
 paru -S bitwarden fluent-reader hardcode-tray pfetch simple-mtpfs spotify obsidian-appimage minecraft-launcher
-
+paru -S libxft-bgra lib32-libxft-bgra
 # required by cryptomator
 #paru -S jdk jdk-adoptopenjdk davfs2 kio-fuse sshfs
 #paru -S cryptomator
