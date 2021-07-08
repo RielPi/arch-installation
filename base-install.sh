d98933e1bbdfca1849db79746ebd7bb5ea032c68
Riel <83147796+RielPi@users.noreply.github.com>
2021-07-08 09:53:20 -0500

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -4,6 +4,7 @@
 echo "[1] Setting timezone (America/Bogota)"
 ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
 hwclock --systohc
+timedatectl set-ntp true
 
 # [2] language
 echo "[2] Editing locale.gen, find en_US.UTF-8 & en_US ISO-8859-1, ja_JP.UTF-8 UTF-8 and uncomment"
